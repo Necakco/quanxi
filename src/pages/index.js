ff90d733fcf9ac4d4ff4adc0d023336ccebf4501
Necakco <khoavencyti@gmail.com>
2023-06-23 22:30:17 +0700

    fn

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Necakco Comunity">
       <HomepageHeader />
       <main>
       </main>
